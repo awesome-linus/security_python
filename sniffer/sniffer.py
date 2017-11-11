4a5293a53b97f8bea0036efb807999436f5bee25
awesome_linus <awesomeblog.linus@gmail.com>
2017-11-11 17:53:03 +0900

    add sniffer

--- sniffer/sniffer.py
+++ sniffer/sniffer.py
@@ -1,11 +1,18 @@
+# -*- coding: utf-8 -*-
+
 import socket
 
+# create object
 sniff = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_ICMP)
 
+# bind data
 sniff.bind(('127.0.0.1', 0))
 
-sniff.setsockopt(socket.IPPROTO_IP,socket.IP_HDRINCL, 1)
+# socket option
+sniff.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
 
+# sniffer is now active
 print('Sniffer is listening!')
 
+# get messages from socket
 print(sniff.recvfrom(4096))
--- sniffer/udpclient.py
+++ sniffer/udpclient.py
@@ -1,9 +1,18 @@
+# -*- coding: utf-8 -*-
+
 import socket
 
-client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-client.sendto(b"12345", ("127.0.0.1",80))
+target_host = "127.0.0.1"
+target_port = 80
 
+# create socket object
+client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+# send data
+client.sendto(b"12345", (target_host, target_port))
+
+# receive data
 data, addr = client.recvfrom(4096)
 
-
+# display on terminal
 print(data)
